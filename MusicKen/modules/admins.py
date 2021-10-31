5a671d2d9f8ee81d2bea4d9dccf06a7ab6b61538
burakizm <88186150+burakizm@users.noreply.github.com>
2021-10-31 13:13:11 +0300

    Create admins.py

---
+++ MusicKen/modules/admins.py
@@ -0,0 +1,102 @@
+
+from asyncio.queues import QueueEmpty
+from MusicKen.config import que
+from MusicKen.config import BOT_USERNAME
+from pyrogram import Client, filters
+from pyrogram.types import Message
+
+from MusicKen.function.admins import set
+from MusicKen.helpers.channelmusic import get_chat_id
+from MusicKen.helpers.decorators import authorized_users_only
+from MusicKen.helpers.decorators import errors
+from MusicKen.helpers.filters import command, other_filters
+from MusicKen.services.callsmusic import callsmusic
+
+
+@Client.on_message(filters.command(["adminreset", f"adminreset@{BOT_USERNAME}"]))
+@authorized_users_only
+async def update_admin(client, message: Message):
+    chat_id = get_chat_id(message.chat)
+    set(
+        chat_id,
+        [
+            member.user
+            for member in await message.chat.get_members(filter="administrators")
+        ],
+    )
+    await message.reply_text("✅️ yönetici ön belleği yenilendi!")
+
+@Client.on_message(command(["durdur", f"pause@{BOT_USERNAME}"]) & other_filters)
+@authorized_users_only
+async def durdur(_, message: Message):
+    chat_id = get_chat_id(message.chat)
+    if (chat_id not in callsmusic.pytgcalls.active_calls) or (
+        callsmusic.pytgcalls.active_calls[chat_id] == "paused"
+    ):
+        await message.reply_text(" Şeker Müsic Bot🎶❗ **şuan seste oynatılan bir music bulunamadı!**")
+    else:
+        callsmusic.pytgcalls.pause_stream(chat_id)
+        await message.reply_text(" Şeker Music Bot :- 🤐 Durduruldu**")
+
+
+@Client.on_message(command(["devam", f"resume@{BOT_USERNAME}"]) & other_filters)
+@authorized_users_only
+async def devam(_, message: Message):
+    chat_id = get_chat_id(message.chat)
+    if (chat_id not in callsmusic.pytgcalls.active_calls) or (
+        callsmusic.pytgcalls.active_calls[chat_id] == "playing"
+    ):
+        await message.reply_text(" Şeker Müsic Bot🎶❗ **oynatılacak şarkı bulunamadı!**")
+    else:
+        callsmusic.pytgcalls.resume_stream(chat_id)
+        await message.reply_text(" Şeker Music Bot :- 🥳 Devam Ediyor!")
+
+
+@Client.on_message(command(["son", f"end@{BOT_USERNAME}"]) & other_filters)
+@authorized_users_only
+async def stop(_, message: Message):
+    chat_id = get_chat_id(message.chat)
+    if chat_id not in callsmusic.pytgcalls.active_calls:
+        await message.reply_text("Şeker Müsic Bot🎶❗ **sonlandırılacak şarkı bulunamadı!**")
+    else:
+        try:
+            callsmusic.queues.clear(chat_id)
+        except QueueEmpty:
+            pass
+
+        callsmusic.pytgcalls.leave_group_call(chat_id)
+        await message.reply_text("Miss Music Bot :- ❌ **Bitti!**")
+
+
+@Client.on_message(command(["atla", f"skip@{BOT_USERNAME}"]) & other_filters)
+@authorized_users_only
+async def atla(_, message: Message):
+    global que
+    chat_id = get_chat_id(message.chat)
+    if chat_id not in callsmusic.pytgcalls.active_calls:
+        await message.reply_text("**Şeker Music  Bot :- 🙄 Atlamak için hiçbir şey oynamıyor!**")
+    else:
+        callsmusic.queues.task_done(chat_id)
+
+        if callsmusic.queues.is_empty(chat_id):
+            callsmusic.pytgcalls.leave_group_call(chat_id)
+        else:
+            callsmusic.pytgcalls.change_stream(
+                chat_id, callsmusic.queues.get(chat_id)["file"]
+            )
+
+        await message.reply_text("Şeker Music Bot :- 😬Bir Sonraki Müziğe Atlandı!")
+
+
+@Client.on_message(filters.command(["admincache", f"admincache@{BOT_USERNAME}"]))
+@authorized_users_only
+async def admincache(client, message: Message):
+    set(
+        message.chat.id,
+        [
+            member.user
+            for member in await message.chat.get_members(filter="administrators")
+        ],
+    )
+    await message.reply_text("✅️ **Yönetici listesi** vardır **güncellenmiş**")
+
